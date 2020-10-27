5cd852b36e9140e25581c7e1ee3a2aa7e4ef6c8c
chenjingsi <saswhite@163.com>
2020-10-27 19:02:54 +0800

    change

--- src/Router/index.js
+++ src/Router/index.js
@@ -1,15 +1,9 @@
 import React from 'react';
 
 /* pages */
-<<<<<<< HEAD
-import Login from '../Features/Login/Login';
-import Restaurant from '../Features/Restaurant/restaurant';
-import Menu from '../Features/Menu/Menu';
-=======
 import Login from '../Features/Login/login';
 import Restaurant from '../Features/Restaurant/Restaurant';
 import Menu from '../Features/Menu/menu';
->>>>>>> 27009dcb4a5611692b960c8845ed99a559b707e4
 import Counter from '../Features/Counter/Counter';
 import Order from '../Features/Order/Order';
 
