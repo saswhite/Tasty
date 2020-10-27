3f15d54e77eb26792328f0fa72136009c9312386
chenjingsi <saswhite@163.com>
2020-10-28 00:38:15 +0800

    transform and header

--- src/features/Menu/menu.js
+++ src/features/Menu/menu.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { useDispatch } from 'react-redux';
 import Modal from '../../Components/Modal/Modal';
 import { showModal } from '../../Redux/Reducer/Modal';
-<<<<<<< HEAD
 
 /* component */
 import Header from '../../Components/Header/Header';
@@ -11,9 +10,6 @@ import Header from '../../Components/Header/Header';
 import './menu.scss';
 
 export default function Menu () {
-=======
-export default function Menu (){
->>>>>>> 1841a7d4db8567c8df57d98bc85a135246c09573
   const dispatch = useDispatch();
   return (
     <div>
