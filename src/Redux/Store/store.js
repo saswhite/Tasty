a5f04c09ff37404c172cc4944c27be85dbf60a24
石雪婷 <shixt@shixuetingdeMacBook-Pro.local>
2020-10-27 19:29:54 +0800

    modal

--- src/Redux/Store/store.js
+++ src/Redux/Store/store.js
@@ -1,8 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
+/* 局部 */
 import counterReducer from '../../Features/Counter/state/reducer';
+/* 全局 */
+import  modalReducer from '../Reducer/Modal';
 
 export default configureStore({
   reducer: {
     counter: counterReducer,
+    modal:modalReducer
   },
 });
