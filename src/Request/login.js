a8ceef29e94be67099646ca106fe11375ed45e98
lxn <edz@LXN.local>
2020-10-28 10:35:18 +0800

    error test login

--- src/Request/login.js
+++ src/Request/login.js
@@ -2,9 +2,9 @@ import request from '../Common/request';
 import { host } from '../Common/config';
 export async function login (){
   const result = await request({
-    url: `${host}/user/login`,
-    method:'post',
-    data:{ username: '9eaf50f13318116d62312b74c765b329', password: '38f554b767255d234fa4b21611d4f603' }
+    url: `${host}/order/5e219e0aa274ef537609fe86`,
+    method:'get',
+    // data:{ username: '9eaf50f13318116d62312b74c765b329', password: '38f554b767255d234fa4b21611d4f603' }
   });
   return result;
 }
