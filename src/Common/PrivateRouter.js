38e19ed8340e683b3fab29c55291996085d9a91d
lxn <edz@LXN.local>
2020-10-29 20:38:09 +0800

    路由跳转 修改

--- src/Common/PrivateRouter.js
+++ src/Common/PrivateRouter.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Route,Redirect } from 'react-router-dom';
+import { getStorage } from './utils';
 
 export default function PrivateRouter ({ component: Component , ...rest }) {
 
   let auth = true;
   if(rest.path === '/'){
     auth = false;
+  }
+  let user = getStorage('user');
+  if(!user){
+    if(rest.path === '/order'){
+
+      auth = false;
+    }
+  }else{
+    if(rest.path === '/login'){
+      auth = false;
+    }
   }
 
   return (
